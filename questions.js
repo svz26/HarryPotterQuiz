3df0a90ec04d02a4fc6d1e5cf436e11cb728c7f7
svz26 <131193513+svz26@users.noreply.github.com>
2024-08-16 23:42:55 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,37 @@
+let questions=[
+    {
+        numb:1,
+        question:"Who are the Muggle aunt and uncle that Harry must live with every summer?",
+        answer:"C. Vernon and Petunia Dursley",
+        options:["A. Ned and Camilla Diddly","B. Ralph and Magnolia Dudley","C. Vernon and Petunia Dursley"
+        ]
+    },
+    {
+        numb:2,
+        question:"In order of birth, who are the seven Weasley siblings?",
+        answer:"C. Bill, Charlie, Percy, Fred, George, Ron, Ginny",
+        options:["A. Percy, Bill, George, Charlie, Fred, Ron, Ginny","B. George, Fred, Percy, Bill, Charlie, Ron, Ginny","C. Bill, Charlie, Percy, Fred, George, Ron, Ginny"
+        ]
+    },
+    {
+        numb:3,
+        question:"Hogwarts students are excited when Professor Lockhart starts a dueling club. At the first meeting, Harry unwittingly reveals a mysterious and rare ability. What is this ability?",
+        answer:"C. Harry can speak Parseltongue.",
+        options:["A. Harry is a Horcrux.","B. Harry can cast a Patronus charm.","C. Harry can speak Parseltongue."
+        ]
+    },
+    {
+        numb:4,
+        question:"What are the three Unforgivable Curses?",
+        answer:"B. Avada Kedavra, Crucio and Imperio",
+        options:["A. Impedimenta, Incarcerous and Incendio","B. Avada Kedavra, Crucio and Imperio","C. Expelliarmus, Expecto Patronum and Diffindo"
+        ]
+    },
+    {
+        numb:5,
+        question:"Which of these Hogwarts professors teaches Transfiguration?",
+        answer:"C.McGonagall",
+        options:["A. Snape","B. Sprout","C. McGonagall"   
+        ]
+    }
+];
